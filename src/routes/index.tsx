c77a34c2cbda5a51dd64f8b92ad024664139eb80
notabeneinc <notabene.inc@gmail.com>
2026-08-14 16:03:59 +0600

    Test monochrome grayscale theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -333,3 +333,8 @@ function HomePage() {
     </div>
   );
 }
+/* পুরো সাইটকে মার্জিত সাদা-কালো (Grayscale) লুকে দেখার ফিল্টার */
+html {
+  filter: grayscale(100%) !important;
+  -webkit-filter: grayscale(100%) !important;
+}
