f579ee459e5097377ab00b98b3c56f403941d150
kaalikolom-ux <kaali.kolom@gmail.com>
2026-08-28 16:27:55 +0600

    chore: purge Lovable error reporting telemetry and 35 unused UI component boilerplate files

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,6 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
 import { PrefsProvider } from "../lib/prefs";
 import { SiteHeader } from "../components/SiteHeader";
 import { SiteFooter } from "../components/SiteFooter";
@@ -45,9 +44,6 @@ function NotFoundComponent() {
 function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
   console.error(error);
   const router = useRouter();
-  useEffect(() => {
-    reportLovableError(error, { boundary: "tanstack_root_error_component" });
-  }, [error]);
 
   return (
     <div className="flex min-h-[70vh] items-center justify-center bg-background px-4">
