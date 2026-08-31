7ca364d461ce4e5b07ae11b0fc004e4e36f03133
kaalikolom-ux <kaali.kolom@gmail.com>
2026-08-31 16:21:18 +0600

    feat(quran): integrate scientific translation and metadata for Surah 2 (Al-Baqarah) ayahs 231 to 237

--- src/routes/surah.$id.tsx
+++ src/routes/surah.$id.tsx
@@ -344,7 +344,7 @@ const applyLocalMetaOverrides = (sId: number, data: SurahData) => {
   return data;
 };
 
-const APP_DATA_VERSION = "20260831_v3";
+const APP_DATA_VERSION = "20260831_v4";
 
 const fetchSurahData = async (sId: number): Promise<SurahData> => {
   // 1. Fetch from static JSON with versioned caching
